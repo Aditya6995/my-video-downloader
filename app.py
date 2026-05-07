a6cb11b397c49434c3156b41ab68b00deebcb0ac
Aditya6995 <gupta6155@gmail.com>
2026-05-07 11:41:10 +0530

    Refactor video downloader with Streamlit integration

--- app.py
+++ app.py
@@ -1,73 +1,26 @@
-ydl_opts = {
-    'format': 'best',
-    'cookiefile': 'cookies.txt',  # Netscape HTTP Cookie File
-# https://curl.haxx.se/rfc/cookie_spec.html
-# This is a generated file! Do not edit.
-
-.youtube.com	TRUE	/	TRUE	1808816514	__Secure-YENID	14.YTE=HAX5Lnk_4-8pbwdHcY7_uFfkqnoNkz2-WawramPvCPHLpK4huGpVOPLxk3K8UNXZtQWKUMuCzimGa2C7NFk_F00i5rT99cgC4Tjo7IECY6e8b_UOgNJj5qUlcSIcxYnxsUssDZV5pzYAIB4MkGqQP3-vlfqNrsPy7feREpxHjcJNVVNu3nZG7maIHPgdmHcO_FrDNKBWmOtd7SAjtizjD6TexZP5Ka-Z3njUBj4NlQ5g7r7ZVlUuqVq6o-iz9BPSKgTkUO2y2ksDS1a96zc4UdXJSFq4Xb3VHf3IGhOJZEWF1y32hyojlUAk-PFnXwARTxvKPC8xsrxAARsUVGqa3Q
-.youtube.com	TRUE	/	TRUE	1791136917	__Secure-BUCKET	CNcG
-.youtube.com	TRUE	/	FALSE	1812616123	HSID	A7qAbRtET3VUlDCKb
-.youtube.com	TRUE	/	TRUE	1812616123	SSID	APsATzwBivXtg5_yh
-.youtube.com	TRUE	/	FALSE	1812616123	APISID	JrC17mPAi6wty5cf/AP9wNm6totAKJFF1V
-.youtube.com	TRUE	/	TRUE	1812616123	SAPISID	McPV-jPar7z83Y-y/Aw0TIgX1DyXHB4btG
-.youtube.com	TRUE	/	TRUE	1812616123	__Secure-1PAPISID	McPV-jPar7z83Y-y/Aw0TIgX1DyXHB4btG
-.youtube.com	TRUE	/	TRUE	1812616123	__Secure-3PAPISID	McPV-jPar7z83Y-y/Aw0TIgX1DyXHB4btG
-.youtube.com	TRUE	/	FALSE	1812616123	SID	g.a0009QgtzH1GZPcI69VjRUsABJqk2M80uMYbxMT5Wp9D-uq4hcDxjgzdaeIX11Z6JRpSh2r8WQACgYKAYoSARISFQHGX2Micxo3ZlbASuOKs7qCev6VtBoVAUF8yKorfB3sqx0vk2XBf7PxBuO20076
-.youtube.com	TRUE	/	TRUE	1812616123	__Secure-1PSID	g.a0009QgtzH1GZPcI69VjRUsABJqk2M80uMYbxMT5Wp9D-uq4hcDx0WypWlttSLJLIX3b506AYAACgYKAdwSARISFQHGX2Miq7ryxWEpIQuJwadNgQ47FxoVAUF8yKqQeLe17wlTJk9sQ6vECP_70076
-.youtube.com	TRUE	/	TRUE	1812616123	__Secure-3PSID	g.a0009QgtzH1GZPcI69VjRUsABJqk2M80uMYbxMT5Wp9D-uq4hcDxKYxvq--q9rhcflBQjuN1NAACgYKAWcSARISFQHGX2MiRIZFggdgwrzaHsmv_46lTBoVAUF8yKq6SfBp7101KcTVKqBO-Ub10076
-.youtube.com	TRUE	/	TRUE	1812616132	LOGIN_INFO	AFmmF2swRAIgHZ4_vrzPvqWQzyQtA6y2Vc2vVo5y20de69FujLhZGHICIBKxZfjuvAaHZkeiE9l96-OCGUmQWa1bvNZMc4cBRqYZ:QUQ3MjNmemxMSENvbUhLNkU2elZvNGpVWGwybTdZTm9pa29wUE1sUVNud1l3Rkx5VElJX21sUVYxMUxzenQzWjUyMFlRb0JoYVA2dWxlUER4eW9TR0hJWFQ5ZDBuQjhlMmxiSW41NklBX09vbGZtYlFzRDF5SkwweWhBdjU2bUVzelZQZENUaUdyUGhkTGhoU1FxUjdTTTcwVkRtOUF1ME9B
-.youtube.com	TRUE	/	TRUE	1812692763	PREF	tz=Asia.Calcutta&f5=20000&f7=100&f6=40000000
-.youtube.com	TRUE	/	TRUE	1809669177	__Secure-1PSIDTS	sidts-CjQBhkeRd8S0A60hMxGcQiZgaukzVhxaKkjiXr3avrzFQIHUB83tmEb21-eykZREW7Qb-W4gEAA
-.youtube.com	TRUE	/	TRUE	1809669177	__Secure-3PSIDTS	sidts-CjQBhkeRd8S0A60hMxGcQiZgaukzVhxaKkjiXr3avrzFQIHUB83tmEb21-eykZREW7Qb-W4gEAA
-.youtube.com	TRUE	/	FALSE	1809669178	SIDCC	AKEyXzWIX0WnBpP3WvEdIb8cp5K5fH2sC1wqz6VtnGmig9G72dYKlWFyeZhS_S3UpKOE_pnsOw
-.youtube.com	TRUE	/	TRUE	1809669178	__Secure-1PSIDCC	AKEyXzU8qqBS0WCBMilOBixu69EyZjMkWQ6LGDhELFCXD2JV2Q1uNhUGJvI0sIA5xMAD5m5NbA
-.youtube.com	TRUE	/	TRUE	1809669178	__Secure-3PSIDCC	AKEyXzVOJJoW6vo-pkRT0Frv52y9tqtvfNkzEMpsJSZh1tfIBzqy2FEpmsd1_DGRMz9vWl6cyw
-.youtube.com	TRUE	/	TRUE	1793684760	VISITOR_INFO1_LIVE	cUX47qo5Ccw
-.youtube.com	TRUE	/	TRUE	1793684760	VISITOR_PRIVACY_METADATA	CgJJThIEGgAgSA%3D%3D
-.youtube.com	TRUE	/	TRUE	1790579827	VISITOR_INFO1_LIVE	SzsSa5u0TeY
-.youtube.com	TRUE	/	TRUE	1790579827	VISITOR_PRIVACY_METADATA	CgJJThIEGgAgbg%3D%3D
-.youtube.com	TRUE	/	TRUE	1791445934	__Secure-ROLLOUT_TOKEN	CIminY6q65m-cBD-q8f4jcyTAxii1Oq4qOWTAw%3D%3D
-.youtube.com	TRUE	/	TRUE	0	YSC	MPpUgbZNRRs
-.youtube.com	TRUE	/	TRUE	1793603877	__Secure-YNID	18.YT=s_SYWPD-xoAuiHKyH40ttWGnjB353QDZr_L7vI2Fixjn_-0HjoWFNhCIR4KNH07EKLVDCfhutZWokdattYEUCFfDLPAZoVWktkiT1EAM7JKBssOcuAGSzbYa51i3QQrHFQHma6cMouZfazpdLebttxgStfB7F0SIaD59WwqiD675hLyXX_Tj5dLRGYrrL4O-NtUr2PSD8q6anbbj7RyW8_ZUY4lLgtErFdbwsdStUj-xAoQaX2G6J4jY-533Xyg21ZxZOWWnyOTVTLdt-Sc4GrGdNWZAFQ-KyQIfszIcrrsI5wRVZKGHs-ZtgU8e_ZU6EDSYbHB1qFqCkuImWgAb7w
-.youtube.com	TRUE	/	TRUE	1793603877	__Secure-ROLLOUT_TOKEN	CKiR3O7SyY2VBhDljPqEh_mRAxjr-Je0j6SUAw%3D%3D
-
-    'outtmpl': 'video.mp4',
-}
+import streamlit as st
+import yt_dlp
+import os
 
 st.set_page_config(page_title="Pro Video Downloader", page_icon="🎥")
 
 st.title("🎥 Ultimate Video Downloader")
 
-url = st.text_input("Video Link Paste Karein:", placeholder="https://www.youtube.com/...")
+url = st.text_input("Video Link Paste Karein:")
 
 if st.button("Download Video"):
     if url:
         try:
-            with st.spinner('Cookies use karke YouTube se permission le rahe hain...'):
-                if os.path.exists("video.mp4"):
-                    os.remove("video.mp4")
-                
+            with st.spinner('Downloading...'):
                 ydl_opts = {
                     'format': 'best',
+                    'cookiefile': 'cookies.txt',  # Yeh line sahi hai yahan
                     'outtmpl': 'video.mp4',
-                    'cookiefile': 'cookies.txt',  # Yeh sabse zaroori line hai
-                    'noplaylist': True,
-                    'quiet': True,
-                    'no_warnings': True,
                 }
-                
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                     ydl.download([url])
                 
                 with open("video.mp4", "rb") as file:
-                    st.success("Download Taiyar Hai!")
-                    st.download_button(
-                        label="Video Save Karein",
-                        data=file,
-                        file_name="video.mp4",
-                        mime="video/mp4"
-                    )
+                    st.download_button("Save Video", data=file, file_name="video.mp4")
         except Exception as e:
-            st.error(f"Dikat: {str(e)}")
-    else:
-        st.warning("Link toh daaliye!")
+            st.error(f"Error: {e}")
